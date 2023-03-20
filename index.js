858e420c9095ec90812e7d82d9ae3b3a57d852f7
John Truong <viajohnt@gmail.com>
2023-03-19 22:43:03 -0600

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(drivers) {
+    return drivers.slice(0,2);
+    //return [drivers[0],drivers[1]];
+}
+function returnLastTwoDrivers(drivers) {
+    return drivers.slice(-2);
+    //return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier) {
+  return function (value) {
+    return value * multiplier;
+  };
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, func) {
+  return func(drivers);
+}
